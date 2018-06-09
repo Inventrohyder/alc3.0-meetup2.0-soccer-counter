5db1dddb88788f810022d410bb7cb2e46a0d1d77
Haitham Alhad Hyder <haitham.hyder@gmail.com>
2018-06-09 16:18:49 +0300

    Resolve errors
    Among many the main one was to pass string values to the views instead
    of ints

--- app/src/main/java/com/example/android/soccercounter/MainActivity.java
+++ app/src/main/java/com/example/android/soccercounter/MainActivity.java
@@ -7,104 +7,100 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
     int goals = 0;
     int reds = 0;
     int yellows = 0;
     int fouls = 0;
 
-    void addGoals(View v){
-        goals += 1;
-        displayGoals(goals);
-    }
-
-    void addReds(View v){
-        reds += 1;
-        displayReds(reds);
-    }
-
-    void addYellows(View v){
-        yellows += 1;
-        displayYellows(yellows);
-    }
-
-    void addFouls(View v){
-        fouls += 1;
-        displayFouls(fouls);
-    }
-
-    void displayGoals(int value){
-        TextView toChange = (TextView) findViewById(R.id.goals_text);
-        toChange.setText(value);
-    }
-
-    void displayReds(int value){
-        TextView toChange = (TextView) findViewById(R.id.reds_text);
-        toChange.setText(value);
-    }
-
-    void displayYellows(int value){
-        TextView toChange = (TextView) findViewById(R.id.yellows_text);
-        toChange.setText(value);
-    }
-
-    void displayFouls(int value){
-        TextView toChange = (TextView) findViewById(R.id.foals_text);
-        toChange.setText(value);
-    }
 
     int goalsb = 0;
     int redsb = 0;
     int yellowsb = 0;
     int foulsb = 0;
 
-    void addGoalsb(View v){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addGoals(View v){
+        goals += 1;
+        displayGoals(goals);
+    }
+
+    public void addReds(View v){
+        reds += 1;
+        displayReds(reds);
+    }
+
+    public void addYellows(View v){
+        yellows += 1;
+        displayYellows(yellows);
+    }
+
+    public void addFoals(View v){
+        fouls += 1;
+        displayFouls(fouls);
+    }
+
+    void displayGoals(int value){
+        TextView toChange = (TextView) findViewById(R.id.goals_text);
+        toChange.setText(String.valueOf(value));
+    }
+
+    void displayReds(int value){
+        TextView toChange = (TextView) findViewById(R.id.reds_text);
+        toChange.setText(String.valueOf(value));
+    }
+
+    void displayYellows(int value){
+        TextView toChange = (TextView) findViewById(R.id.yellows_text);
+        toChange.setText(String.valueOf(value));
+    }
+
+    void displayFouls(int value){
+        TextView toChange = (TextView) findViewById(R.id.foals_text);
+        toChange.setText(String.valueOf(value));
+    }
+
+    public void addGoalsb(View v){
         goalsb += 1;
-        displayGoals(goalsb);
+        displayGoalsb(goalsb);
     }
 
-    void addRedsb(View v){
+    public void addRedsb(View v){
         redsb += 1;
-        displayGoals(redsb);
+        displayRedsb(redsb);
     }
 
-    void addYellowsb(View v){
+    public void addYellowsb(View v){
         yellowsb += 1;
-        displayGoals(yellowsb);
+        displayYellowsb(yellowsb);
     }
 
-    void addFoulsb(View v){
+    public void addFoalsb(View v){
         foulsb += 1;
-        displayGoalsb(foulsb);
-    }
-
-    void displayValueb(int value, int id){
-        TextView toChange = (TextView) findViewById(id);
-        toChange.setText(value);
+        displayFoulsb(foulsb);
     }
 
     void displayGoalsb(int value){
-        TextView toChange = (TextView) findViewById(R.id.goals_text);
-        toChange.setText(value);
+        TextView toChange = (TextView) findViewById(R.id.goals_textb);
+        toChange.setText(String.valueOf(value));
     }
 
     void displayRedsb(int value){
-        TextView toChange = (TextView) findViewById(R.id.reds_text);
-        toChange.setText(value);
+        TextView toChange = (TextView) findViewById(R.id.reds_textb);
+        toChange.setText(String.valueOf(value));
     }
 
     void displayYellowsb(int value){
-        TextView toChange = (TextView) findViewById(R.id.yellows_text);
-        toChange.setText(value);
+        TextView toChange = (TextView) findViewById(R.id.yellows_textb);
+        toChange.setText(String.valueOf(value));
     }
 
     void displayFoulsb(int value){
         TextView toChange = (TextView) findViewById(R.id.foals_textb);
-        toChange.setText(value);
+        toChange.setText(String.valueOf(value));
     }
 }
